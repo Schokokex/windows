52cdf30409c46e235ce3ca8d00c0b48040ea5aa7
Joachim <47549862+Schokokex@users.noreply.github.com>
2022-02-10 19:07:18 +0000

    Update ActivateWSL.cmd

--- ActivateWSL.cmd
+++ ActivateWSL.cmd
@@ -1,1 +1,1 @@
- powershell -Command "Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux | Enable-WindowsOptionalFeature -Online"
+powershell -Command "Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux | Enable-WindowsOptionalFeature -Online" || echo An Error occured. Maybe run again as admin helps && pause
