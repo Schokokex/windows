cb750deb67989ecb7b315bf81a62bd088b8a871b
Joachim <47549862+Schokokex@users.noreply.github.com>
2022-02-10 18:57:24 +0000

    Create InstallAlpine.cmd

---
+++ InstallAlpine.cmd
@@ -0,0 +1,1 @@
+powershell -Command "Add-AppxPackage -Path http://tlu.dl.delivery.mp.microsoft.com/filestreamingservice/files/da187f32-a9b0-4a97-a3bf-4f320f5021ab?P1=1644519962&P2=404&P3=2&P4=h0pr5hpggicgxeV5%2bvKIKADeb6P%2fDb7Tf6ebUHB8gORjEjTsJ2b%2f8OTPdvvGoM%2bSubwwD%2bXQAePk1Rjzjf%2foAg%3d%3d"
