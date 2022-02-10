987f49ba9ba885b97711ce19bc8abadd9f33d79c
Joachim <47549862+Schokokex@users.noreply.github.com>
2022-02-10 22:45:12 +0000

    Update RemoveCapabilities.cmd

--- RemoveCapabilities.cmd
+++ RemoveCapabilities.cmd
@@ -1,1 +1,1 @@
-powershell -Command "Get-WindowsCapability -Online | Where-Object {$_.State -Match 'Installed'} | Where-Object {$_.Name -NotMatch 'Basic|TextToSpeech'} | Remove-WindowsCapability" || echo run as admin && pause
+powershell -Command "Get-WindowsCapability -Online | Where-Object {$_.State -Match 'Installed'} | Where-Object {$_.Name -NotMatch 'Basic|TextToSpeech'} | Remove-WindowsCapability -Online" || echo run as admin && pause
