c569a8b856a788bc2f9a7744232d55a4f4396183
Joachim <47549862+Schokokex@users.noreply.github.com>
2022-02-10 19:21:51 +0000

    Create InstallFirefox.cmd

---
+++ InstallFirefox.cmd
@@ -0,0 +1,2 @@
+IF %username% EQU Administrator (echo running as Administrator... && pause)
+powershell -Command "Add-AppxPackage -Path 'http://tlu.dl.delivery.mp.microsoft.com/filestreamingservice/files/172e5469-fb45-47e6-b543-62507081d100?P1=1644523744&P2=404&P3=2&P4=ghAOkBIVCuSw7k4wpWx8lBNUEFf9cfOzYvE%2bJ9G3jP5%2fjJ6K2Z%2fsInPIEx%2bvoblAQWXjZvoY5%2f%2b39oyBNXhReg%3d%3d'"
