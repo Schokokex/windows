e65af2df7e0c9a4c9e54939f766129ed2073bce5
Joachim <47549862+Schokokex@users.noreply.github.com>
2022-02-10 12:58:16 +0000

    Create postinstall.cmd

---
+++ postinstall.cmd
@@ -0,0 +1,23 @@
+:: flaws: not declarative, that means it could be in an undefined state if the pc crashes during execution
+
+
+:: Fresh Windows -> set Admin password -> activate admin -> unsudo current user -> require current user password -> add to remote desktop users -> deactivate all Optional Features -> Enable Sandbox, WSL, pdf -> uninstall all apps except Windows Store
+
+
+:: start "Set Administrator Password" net user Administrator * && net user Administrator /active:yes
+:: start "Set User Password" net user A *
+
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+echo 
