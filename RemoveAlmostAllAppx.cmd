fa991469d767c11e53b3fa16beaa1977457fb7b6
Joachim <47549862+Schokokex@users.noreply.github.com>
2022-02-12 01:37:30 +0100

    Update RemoveAlmostAllAppx.cmd

--- RemoveAlmostAllAppx.cmd
+++ RemoveAlmostAllAppx.cmd
@@ -1,2 +1,3 @@
+::still throws an error
 echo "make sure to run this as admin" && pause
 powershell -Command "Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -NotMatch 'DesktopAppInstaller|WindowsStore'} | Remove-AppxProvisionedPackage -Online -AllUsers"
